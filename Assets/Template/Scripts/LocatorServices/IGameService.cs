31eee6800b01ce1d2698ff6d4c28b0a7b191a43c
Alexandr Fandeev <alexandr.fandeev2001@gmail.com>
2023-01-16 17:27:36 +0200

    Fully edited project for better render

--- Assets/Template/Scripts/LocatorServices/IGameService.cs
+++
@@ -1,7 +0,0 @@
-namespace _Project.Scripts.Core.LocatorServices
-{
-    public interface IGameService
-    {
-        public void Register();
-    }
-}
--- Assets/Template/Scripts/SignalBus/Sub.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace _Project.Scripts.Core.SignalBus
-{
-    [AttributeUsage(AttributeTargets.Method)]
-    public class Sub : Attribute
-    {
-    }
-}

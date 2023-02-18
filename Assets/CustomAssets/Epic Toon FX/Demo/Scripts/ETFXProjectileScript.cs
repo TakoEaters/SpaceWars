98082035d874186b6bd4542192c30bc7019460d3
Alexandr Fandeev <alexandr.fandeev2001@gmail.com>
2023-02-19 00:55:26 +0200

    updated weapon

--- Assets/CustomAssets/Epic Toon FX/Demo/Scripts/ETFXProjectileScript.cs
+++ Assets/CustomAssets/Epic Toon FX/Demo/Scripts/ETFXProjectileScript.cs
@@ -6,6 +6,8 @@ namespace CustomAssets.Epic_Toon_FX.Demo.Scripts
 {
     public class ETFXProjectileScript : MonoBehaviour
     {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private SphereCollider _sphereCollider;
       //  public GameObject impactParticle; // Effect spawned when projectile hits a collider
      //   public GameObject projectileParticle; // Effect attached to the gameobject as child
      //   public GameObject muzzleParticle; // Effect instantly spawned when gameobject is spawned
@@ -32,25 +34,22 @@ namespace CustomAssets.Epic_Toon_FX.Demo.Scripts
 		
         void FixedUpdate()
         {	
-			if (GetComponent<Rigidbody>().velocity.magnitude != 0)
+			if (_rigidbody.velocity.magnitude != 0)
 			{
-			    transform.rotation = Quaternion.LookRotation(GetComponent<Rigidbody>().velocity); // Sets rotation to look at direction of movement
+			    transform.rotation = Quaternion.LookRotation(_rigidbody.velocity); // Sets rotation to look at direction of movement
 			}
 			
             RaycastHit hit;
 			
             float radius; // Sets the radius of the collision detection
-            if (transform.GetComponent<SphereCollider>())
-                radius = transform.GetComponent<SphereCollider>().radius;
-            else
-                radius = colliderRadius;
+            if (_sphereCollider) radius = _sphereCollider.radius;
+            else radius = colliderRadius;
 
-            Vector3 direction = transform.GetComponent<Rigidbody>().velocity; // Gets the direction of the projectile, used for collision detection
-            if (transform.GetComponent<Rigidbody>().useGravity)
-                direction += Physics.gravity * Time.deltaTime; // Accounts for gravity if enabled
+            Vector3 direction = _rigidbody.velocity; // Gets the direction of the projectile, used for collision detection
+            if (_rigidbody.useGravity) direction += Physics.gravity * Time.deltaTime; // Accounts for gravity if enabled
             direction = direction.normalized;
 
-            float detectionDistance = transform.GetComponent<Rigidbody>().velocity.magnitude * Time.deltaTime; // Distance of collision detection for this frame
+            float detectionDistance = _rigidbody.velocity.magnitude * Time.deltaTime; // Distance of collision detection for this frame
 
             if (Physics.SphereCast(transform.position, radius, direction, out hit, detectionDistance)) // Checks if collision will happen
             {
@@ -58,19 +57,6 @@ namespace CustomAssets.Epic_Toon_FX.Demo.Scripts
 
                 GameObject impactP = Instantiate(_projectile.CurrentFX.HitParticle.gameObject, transform.position, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject; // Spawns impact effect
 
-                ParticleSystem[] trails = GetComponentsInChildren<ParticleSystem>(); // Gets a list of particle systems, as we need to detach the trails
-                //Component at [0] is that of the parent i.e. this object (if there is any)
-                for (int i = 1; i < trails.Length; i++) // Loop to cycle through found particle systems
-                {
-                    ParticleSystem trail = trails[i];
-
-                    if (trail.gameObject.name.Contains("Trail"))
-                    {
-                        trail.transform.SetParent(null); // Detaches the trail from the projectile
-                        Destroy(trail.gameObject, 2f); // Removes the trail after seconds
-                    }
-                }
-                
                 _draw.OnPaint(hit.collider.gameObject, hit.point);
                 GetComponent<Projectile>().DetectTarget(hit.collider.gameObject, 25);
 

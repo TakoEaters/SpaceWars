846588d6c4bd70b889748d44d2d4544ca395d217
Alexandr Fandeev <alexandr.fandeev2001@gmail.com>
2023-02-21 15:53:45 +0200

    added icon

--- Assets/_Project/Scripts/GUi/MainMenu/PanelsHandler/Panels/SettingsPanel.cs
+++ Assets/_Project/Scripts/GUi/MainMenu/PanelsHandler/Panels/SettingsPanel.cs
@@ -1,5 +1,6 @@
 using _Project.Scripts.Core.SignalBus;
 using _Project.Scripts.GUi.MainMenu.NavigationSystem;
+using DG.Tweening;
 using Template.Scripts.General;
 using UnityEngine;
 
@@ -8,8 +9,11 @@ namespace _Project.Scripts.GUi.MainMenu.PanelsHandler.Panels
     public class SettingsPanel : PanelHandler
     {
         [SerializeField] private View _panelView;
+        [SerializeField, Range(0.1f, 1.0f)] private float _animationDuration = 0.35f;
         
         public override NavigationTab Navigation => NavigationTab.Settings;
+
+        private const float MaxScale = 1f;
         
         [Sub]
         protected override void OnNavigate(Navigate reference)
@@ -17,7 +21,12 @@ namespace _Project.Scripts.GUi.MainMenu.PanelsHandler.Panels
             if (reference.Destination != NavigationTab.Settings)
             {
                 _panelView.Disable();
+                return;
             }
+            
+            _panelView.Enable();
+            _panelView.transform.localScale = Vector3.zero;
+            _panelView.transform.DOScale(MaxScale, _animationDuration);
         }
     }
 }

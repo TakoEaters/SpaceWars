641593b0e14718b11fa6adfcc8b2e5d45053240d
Alexandr Fandeev <alexandr.fandeev2001@gmail.com>
2023-02-26 22:03:53 +0200

    increased player and AI sped

--- Assets/_Project/Scripts/Common/LevelsManager.cs
+++ Assets/_Project/Scripts/Common/LevelsManager.cs
@@ -1,5 +1,6 @@
 using _Project.Scripts.Core.LocatorServices;
 using _Project.Scripts.GUi;
+using CrazyGames;
 
 namespace _Project.Scripts.Common
 {
@@ -7,11 +8,14 @@ namespace _Project.Scripts.Common
     {
         public static void LoadMainMenu()
         {
+            CrazyEvents.Instance.GameplayStop();
             ServiceLocator.Current.Get<ILoadingScene>().Load(1);
         }
 
         public static void LoadGame()
         {
+            ServiceLocator.Current.Get<IAdsManager>().ShowInterstitial();
+            CrazyEvents.Instance.GameplayStart();
             ServiceLocator.Current.Get<ILoadingScene>().Load(2);
         }
     }
